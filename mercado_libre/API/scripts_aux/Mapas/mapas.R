9dc0639ccabc64d68a497f0216f75c968c213dc8
unknown <luciacoudet@gmail.com>
2021-06-13 20:00:56 -0300

    actualizacion mapas

--- mercado_libre/API/scripts_aux/Mapas/mapas.R
+++ mercado_libre/API/scripts_aux/Mapas/mapas.R
@@ -10,11 +10,10 @@ library(usethis)
 library(sp)
 library(scales) 
 library(geosphere) #para medir distancias
+library(RecordLinkage) #funcion para march palabras
 
 # Funciones auxiliares
-
 source(here("mercado_libre/API/funciones","funcion_transf_apt.R"))
-
 
 barrios_aptos <- list.files(path = here("mercado_libre/API/datos/apt/202106"), pattern = "*.csv", full.names = T)
 
@@ -24,16 +23,13 @@ datos <- sapply(barrios_aptos, FUN=function(id_barrio){
 
 aptos <- transf_apt(datos)[["aptos"]]
 
-
-#filtramos lat y long valores que tienen sentido
-#aptos <- aptos %>% filter(latitude < -30, longitude < -50, longitude > -60)
-
 #Leemos vctorial INE
-mapa_barrio <- st_read("mercado_libre/API/script_aux/Mapas/vectorial_INE_barrios/ine_barrios")
+mapa_barrio <- st_read("mercado_libre/API/scripts_aux/Mapas/vectorial_INE_barrios/ine_barrios")
+
 #Pasa geometría a formato longlat 
 mapa_barrio <- st_transform(mapa_barrio, '+proj=longlat +zone=21 +south +datum=WGS84 +units=m +no_defs')
 
-### match city names - mapa barrio
+# Match city names base aptos - NOMBARR base mapa_barrio
 aptos_city <- levels(as.factor(aptos$city_name))
 aptos_city <- as.data.table(aptos_city) %>% 
       rename(city_name = aptos_city) %>%
@@ -41,33 +37,39 @@ aptos_city <- as.data.table(aptos_city) %>%
       mutate(city_name2 = city_name)
 
 
-for (i in 1:nrow(aptos_city)) {
-      aptos_city[i, 2] = ifelse(length(agrep(aptos_city[i,1], mapa_barrio$NOMBBARR, ignore.case = TRUE, value = TRUE)) > 0,
-                             agrep(aptos_city[i,1], mapa_barrio$NOMBBARR, ignore.case = TRUE, value = TRUE)[1],
-                             aptos_city[i,2])
+for(i in 1:nrow(aptos_city)) {
+      aux_dist <- jarowinkler(tolower(aptos_city$city_name[i]),
+                              tolower(mapa_barrio$NOMBBARR))
+      if (max(aux_dist) >= 0.85){
+            aptos_city[i,2] <- mapa_barrio$NOMBBARR[which.max(aux_dist)]
+      }
 }
-
 
 #vemos los que no matchearon y lo hacemos manual
 aptos_city %>% filter(NOMBBARR == 'no match')
-aptos_city$city_name2 <- recode(aptos_city$city_name2, 'Jardines Hipódromo' = 'Jardines del Hipodromo',
-                                    'Maroñas, Curva'= 'Flor de Maroñas',
-                                    'Montevideo' = 'Centro',
-                                    'Parque Batlle'='Pque. Batlle, V. Dolores',
-                                    'Pocitos Nuevo' = 'Pocitos',
-                                    'Puerto Buceo' = 'Buceo',
-                                    'Punta Rieles'='Pta. Rieles, Bella Italia',
-                                    'Perez Castellanos' = 'Castro, P. Castellanos',
-                                    'Villa Dolores'='Pque. Batlle, V. Dolores',
-                                    'Villa Biarritz' = 'Punta Carretas',
-                                    'Arroyo Seco' = 'Reducto')
+aptos_city$city_name2 <- recode(aptos_city$city_name2, 
+                                'Arroyo Seco' = 'Aguada',
+                                'Bella Vista' = 'Reducto',
+                                'Bolivar' = 'Mercado Modelo, Bolivar',
+                                'Colón' = 'Colon Centro y Noroeste',
+                                'Goes' = 'Aguada',
+                                'Montevideo' = 'Centro',
+                                'Parque Batlle'='Pque. Batlle, V. Dolores',
+                                'Paso Molino' = 'Belvedere',
+                                'Perez Castellanos' = 'Castro, P. Castellanos',
+                                'Puerto Buceo' = 'Buceo',
+                                'Villa Dolores'='Pque. Batlle, V. Dolores',
+                                'Punta Rieles'='Pta. Rieles, Bella Italia',
+                                'Villa Biarritz' = 'Punta Carretas')
 
-aptos_city <- aptos_city %>% 
-      mutate(NOMBBARR = ifelse(NOMBBARR == 'no match', city_name2, NOMBBARR)) %>%
+aptos_city <- aptos_city %>% #punta rieles lo toma como punta carretas - corregimos
+      mutate(NOMBBARR = ifelse(NOMBBARR == 'no match' | city_name == 'Punta Rieles',
+                               city_name2, NOMBBARR)) %>%
       dplyr::select(-city_name2)
 
 aptos <- aptos %>% left_join(aptos_city, by ='city_name')
 
+#MAPA SEGUN MEDIA
 
 m <- aptos %>% 
       group_by(NOMBBARR) %>% 
@@ -81,13 +83,13 @@ mapa_barrio <- mapa_barrio %>%
       arrange(desc(price)) %>% 
       mutate(precio_pormil = round(price/1000,0))
 
-#PLOTS
+#Plots
 
 ggplot(mapa_barrio)+
       geom_sf(aes(fill=precio_pormil)) +
       geom_sf_text(data = mapa_barrio, aes(label = NOMBBARR), 
                    color = 'white', fill = 'gray', size = 3) +
-      coord_sf(xlim=c(-56.2,-56.05), ylim = c(-34.94, -34.86)) +
+      coord_sf(xlim=c(-56.2,-56.04), ylim = c(-34.94, -34.85)) +
       xlab('Longitud') +
       ylab('Latitud') +
       theme(axis.text.x = element_text(angle = 45),
@@ -108,6 +110,8 @@ ggplot(mapa_barrio)+
       geom_sf(aes(fill=precio_pormil)) +
       xlab('Longitud') +
       ylab('Latitud') +
+      geom_sf_text(data = mapa_barrio, aes(label = NOMBBARR), 
+                   color = 'white', fill = 'gray', size = 3) +
       theme(axis.text.x = element_text(angle = 45),
             text = element_text(family = 'Avenir'),
             panel.grid.major = element_line(
@@ -120,6 +124,68 @@ ggplot(mapa_barrio)+
             legend.title = element_text(size = 9, face = 'bold', hjust = 0.5)) +
       scale_fill_gradient(low = 'firebrick', high = 'darkgreen', name = "Precio promedio \n por mil",
                           labels = comma)
+
+
+
+#MAPA SEGUN MEDIANA
+
+m2 <- aptos %>% 
+      group_by(NOMBBARR) %>% 
+      summarise(price=median(as.numeric(price), na.rm=TRUE)) %>%
+      arrange(desc(price))
+
+mapa_barrio <- mapa_barrio %>% select(-price) %>%
+      left_join(m2, by="NOMBBARR") %>% 
+      arrange(desc(price)) %>% 
+      mutate(precio_pormil = round(price/1000,0))
+
+#Plots
+
+ggplot(mapa_barrio)+
+      geom_sf(aes(fill=precio_pormil)) +
+      geom_sf_text(data = mapa_barrio, aes(label = NOMBBARR), 
+                   color = 'white', fill = 'gray', size = 3) +
+      coord_sf(xlim=c(-56.3,-56.04), ylim = c(-34.94, -34.80)) +
+      xlab('Longitud') +
+      ylab('Latitud') +
+      theme(axis.text.x = element_text(angle = 45),
+            text = element_text(family = 'Avenir'),
+            panel.grid.major = element_line(
+                  color = '#cccccc',linetype = 'dashed',size = .3),
+            panel.background = element_rect(fill = 'aliceblue'),
+            axis.title = element_blank(),
+            axis.text = element_text(size = 8),
+            legend.position = 'bottom',
+            legend.text = element_text(angle = 0, size = 7),
+            legend.title = element_text(size = 9, face = 'bold', hjust = 0.5)) +
+      scale_fill_gradient(low = 'firebrick', high = 'darkgreen', name = "Mediana \n por mil",
+                          labels = comma)
+
+
+ggplot(mapa_barrio)+
+      geom_sf(aes(fill=precio_pormil)) +
+      xlab('Longitud') +
+      ylab('Latitud') +
+      geom_sf_text(data = mapa_barrio, aes(label = NOMBBARR), 
+                   color = 'white', fill = 'gray', size = 3) +
+      theme(axis.text.x = element_text(angle = 45),
+            text = element_text(family = 'Avenir'),
+            panel.grid.major = element_line(
+                  color = '#cccccc',linetype = 'dashed',size = .3),
+            panel.background = element_rect(fill = 'aliceblue'),
+            axis.title = element_blank(),
+            axis.text = element_text(size = 8),
+            legend.position = 'bottom',
+            legend.text = element_text(angle = 0, size = 7),
+            legend.title = element_text(size = 9, face = 'bold', hjust = 0.5)) +
+      scale_fill_gradient(low = 'firebrick', high = 'darkgreen', name = "Mediana \n por mil",
+                          labels = comma)
+
+
+
+
+
+
 
 #Otro formato para etiquetas:
 #geom_sf_label(data = mapa_barrio[1:30,], aes(label = NOMBBARR), 
@@ -145,12 +211,12 @@ ggplot(mapa_barrio)+
 
 #distm(c(lon1, lat1), c(lon2, lat2), fun = distHaversine)
 
-library(maptools)
+#library(maptools)
 
-distm(x = c(aptos$longitude[1],aptos$latitude[1]),
-     y = c(aptos$longitude[2],aptos$latitude[1]),
-     fun = distHaversine)
+#distm(x = c(aptos$longitude[1],aptos$latitude[1]),
+ #    y = c(aptos$longitude[2],aptos$latitude[1]),
+  #   fun = distHaversine)
 
-tkml <- getKMLcoordinates(kmlfile="mykml.kml", ignoreAltitude=T)
+#tkml <- getKMLcoordinates(kmlfile="mykml.kml", ignoreAltitude=T)
 
 
