f5bc250199750f0f12ee26ef882af27ed2a1c1f7
Alvaro <alvaro3432@gmail.com>
2021-10-06 20:59:34 -0300

    Arbol con missranger

--- mercado_libre/API/scripts_aux/aux_arbol.R
+++ mercado_libre/API/scripts_aux/aux_arbol.R
@@ -1,6 +1,5 @@
-################
-#### MODELS ####
-################
+# Script donde se trabaja con arboles de regresion tanto
+# para imputacion por la media como por missranger
 
 rm(list=ls())
 
@@ -13,7 +12,6 @@ library(rpart)
 library(rpart.plot)
 library(rattle)
 library(ranger)
-library(missRanger)
 
 options(scipen = 999)
 
@@ -56,6 +54,8 @@ p_na <- sapply(aptos, function(x) round(sum(is.na(x))/length(x),4)) %>% data.fra
 
 aptos_sin_na <- imput_media(aptos,p=.1)
 
+### Imputacion por media
+
 ##### Arbol de regresion
 
 set.seed(1234)
@@ -96,6 +96,51 @@ arbol.prune <- rpart(price ~ . , data = train,method="anova",
 
 rpart.plot(arbol.prune,roundint = T,digits = 4)
 
+#### Imputacion por missranger
+
+# Cargamos los datos que estan imputados con missRanger 
+
+aptos_mr <- read_csv(here("mercado_libre/API/datos/limpios/apt","aptos_mr.csv"))
+
+aptos_mr <- aptos_mr %>% mutate_if(is.character, as.factor)
+
+# Definimos mismo conjunto de entrenamiento y testeo
+
+train_mr <- aptos_mr[ids,]
+test_mr <- aptos_mr[-ids,]
+
+set.seed(1234)
+arbol_mr <- rpart(price~ . , data=train_mr)
+
+summary(arbol_mr)
+
+# Proceso de poda
+
+broom::tidy(arbol_mr$cptable)
+
+# Grafico de la evolucion del error
+
+cp_error_mr <- data.frame(arbol_mr$cptable)
+
+cp_error_mr %>% ggplot(aes(x=CP,y=xerror))+geom_point(color="red")+geom_line()
+
+# Otra forma
+
+plotcp(arbol_mr) 
+
+# Obtengamos el cp
+
+cp_opt_mr <- arbol_mr$cptable[which.min(arbol_mr$cptable[,"xerror"]),"CP"]
+
+# Arbol podado:
+
+arbol.prune.mr <- rpart(price ~ . , data = train_mr,method="anova",
+                     control=rpart.control(cp=cp_opt_mr))
+
+# Grafico
+
+rpart.plot(arbol.prune.mr,roundint = T,digits = 4)
+
 
 
 ## Guardamos los modelos
@@ -103,4 +148,11 @@ rpart.plot(arbol.prune,roundint = T,digits = 4)
 save(file="arbol_train.RDS",arbol)
 
 save(file="arbol_prune_train.RDS",arbol.prune)
+
+save(file="arbol_train_mr.RDS",arbol_mr)
+
+save(file="arbol_prune_train_mr.RDS",arbol.prune.mr)
+
+
+
 
